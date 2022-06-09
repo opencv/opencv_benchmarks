6f0c4450e0e21e057f4a11955c4003b45a5c3ab5
Kataev Victor <kataev.victor.1234@gmail.com>
2022-06-09 10:46:46 +0300

    Update

--- calibration_with_blender/render.py
+++ calibration_with_blender/render.py
@@ -1,35 +1,106 @@
 import bpy
+from bpy_extras.object_utils import world_to_camera_view
+import random
+import time
 
-c = bpy.data.objects[0]
 
-c.location.x = 0
-c.location.y = 0
-c.location.z = 8
+def check_projection(cam, obj):
+    scene = bpy.context.scene
+    render = scene.render
+    
+    #print(obj.data.vertices[0].co, "- Vert 0 (original)")  
+    
+    # Convert vertices to mesh
+    me = obj.to_mesh()
+    #print(me.vertices[0].co, " - Vert 0 (deformed/modified)")
 
-c.rotation_euler[0] = 0
-c.rotation_euler[1] = 0
-c.rotation_euler[2] = 0
+    # Transform mesh according to translation and rotation
+    me.transform(obj.matrix_world)
+    #print(me.vertices[0].co, " - Vert 0 (deformed/modified, world space)")
 
-p = bpy.data.objects[2]
 
-p.location.x = 0
-p.location.y = 0
-p.location.z = 0
+    # Collect mesh coordinates
+    verts = [vert.co for vert in me.vertices]
+    print(list(verts))
+    
+    # Convert to normalized device coordinates
+    coords_2d = [world_to_camera_view(scene, cam, coord) for coord in verts]
+   
+    
+    # x, y must be in [0, 1], z > 0
+    for x, y, z in coords_2d:
+        print(x, y, z)
+        if x < 0 or x > 1:
+            return False
+        if y < 0 or y > 1:
+            return False
+        if z <= 0:
+            return False
+        
+    return True
+    
+    
+def set_position_origin(obj):
+    obj.location.x = 0
+    obj.location.y = 0
+    obj.location.z = 2
 
-p.rotation_euler[0] = 0
-p.rotation_euler[1] = 0
-p.rotation_euler[2] = 0
+    obj.rotation_euler[0] = 0
+    obj.rotation_euler[1] = 0
+    obj.rotation_euler[2] = 0
 
+
+
+# Get camera
+c = bpy.data.objects['Camera']
+
+# Set camera intrincs
+c.lens_unit = 'MILLIMETERS'
+c.lens = 20 # focus length
+
+# Set camera position
+set_position_origin(c)
+c.location.z = 1
+
+
+# Set pattern init position
+p = bpy.data.objects['checkerboard']
+
+set_position_origin(p)
+
+N = 30 # Number of genrated images
+n = 0
 i = 0
-for a in [0, 0.25, 0.5]:
-    p.rotation_euler[0] = a
-    for x in [-1, 0, 1]:
-        c.location.x = x
 
-        for y in [-2, -1, 0, 1, 2]:
-            c.location.y = y
+random.seed(1)
 
-            bpy.context.scene.render.filepath = '/tmp/render-{:03d}.jpg'.format(i)
-            bpy.ops.render.render(write_still=True)
+while n < N and i < 1000:
+    # Set position
+    p.location.x = random.uniform(-0.4, 0.4)
+    p.location.y = random.uniform(-0.4, 0.4)
+    p.location.z = random.uniform(-0.2, 0.2)
+    
+    # Set rotation
+    p.rotation_euler[0] = random.uniform(0, 0.5)
+    p.rotation_euler[1] = random.uniform(0, 0.5)
+    p.rotation_euler[2] = random.uniform(0, 0.5)
+    
+    # Update matrices
+    bpy.context.view_layer.update()
+    
+    # Debug
+    print('>',i,'<')
+    print(p.location)
+    print(p.rotation_euler)
+    
+    # Render and save image if it fully visible
+    if check_projection(c, p):
+        print('True')
+        bpy.context.scene.render.filepath = '/tmp/render-{:03d}.jpg'.format(i)
+        bpy.ops.render.render(write_still=True)
+        n += 1
 
-            i += 1
+    else:
+        print('False')
+        
+    i +=1 
