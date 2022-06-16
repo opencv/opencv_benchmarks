5039a0af750951c3bd37d62e9c3dd954ca4aec75
Kataev Victor <kataev.victor.1234@gmail.com>
2022-06-16 21:06:55 +0300

    Update

--- calibration_with_blender/render.py
+++ calibration_with_blender/render.py
@@ -1,7 +1,11 @@
 import bpy
 from bpy_extras.object_utils import world_to_camera_view
+import os
 import random
 from math import pi
+import pathlib
+import json
+
 
 
 def check_projection(cam, obj):
@@ -49,64 +53,93 @@ def set_position_origin(obj):
     obj.rotation_euler[1] = 0
     obj.rotation_euler[2] = 0
 
+def hide_objects(names):
+    for name in names:
+        bpy.data.objects[name].location.z = 10
 
 
-# Get camera
-c = bpy.data.objects['Camera']
+if __name__ == '__main__':
+    # Get camera
+    c = bpy.data.objects['Camera']
 
-# Set camera intrincs
-#c.lens_unit = 'MILLIMETERS'
-#c.lens = 20 # focus length
+    # Set camera intrincs
+    #c.lens_unit = 'MILLIMETERS'
+    #c.lens = 20 # focus length
 
-# Set camera position
-set_position_origin(c)
-c.location.z = 0.5
-
-
-# Set pattern init position
-bpy.data.objects['checkerboard'].locaition.z = 10
-bpy.data.objects['circles'].location.z = 10
-bpy.data.objects['radon'].location.z = 10
+    # Set camera position
+    set_position_origin(c)
+    c.location.z = 0.5
 
 
+    # Settings
+    work_dir = '/home/xperience/development/datasets'
+    patterns = ['checkerboard', 'circles', 'radon']
+    N = 5 # Number of genrated images
 
-pattern = 'radon'
-N = 50 # Number of genrated images
-n = 0
-i = 0
+    for pattern in patterns:
+        hide_objects(patterns)
 
-p = bpy.data.objects[pattern]
-set_position_origin(p)
-
-random.seed(1)
-
-while n < N and i < 100000:
-    # Set position
-    p.location.x = random.uniform(-0.3, 0.3)
-    p.location.y = random.uniform(-0.2, 0.2)
-    p.location.z = random.uniform(0, 0.15)
+        p = bpy.data.objects[pattern]
+        set_position_origin(p)
     
-    # Set rotation
-    p.rotation_euler[0] = random.uniform(-0.4, 0.4)
-    p.rotation_euler[1] = random.uniform(-0.4, 0.4)
-    p.rotation_euler[2] = random.uniform(0.7 * pi/2, 1.3 * pi/2)
+        # Fix random seed
+        random.seed(1)
     
-    # Update matrices
-    bpy.context.view_layer.update()
+        # Create pattern directory
+        pattern_dir = os.path.join(work_dir, pattern)
+        pathlib.Path(pattern_dir).mkdir(parents=True, exist_ok=True)
     
-    # Debug
-    print('>', i, ' ' , n, '<')
-    print(p.location)
-    print(p.rotation_euler)
-    
-    # Render and save image if it fully visible
-    if check_projection(c, p):
-        print('True')
-        bpy.context.scene.render.filepath = '/tmp/render-{:03d}.jpg'.format(i)
-        bpy.ops.render.render(write_still=True)
-        n += 1
-
-    else:
-        print('False')
+        dataset_info = {}
+        camera_info = {'focus_length': 50}
+        renders_info = {}
         
-    i +=1 
+        n = 0
+        i = 0
+
+        while n < N and i < 100000:
+            # Set position
+            p.location.x = random.uniform(-0.3, 0.3)
+            p.location.y = random.uniform(-0.2, 0.2)
+            p.location.z = random.uniform(0, 0.15)
+    
+            # Set rotation
+            p.rotation_euler[0] = random.uniform(-0.4, 0.4)
+            p.rotation_euler[1] = random.uniform(-0.4, 0.4)
+            p.rotation_euler[2] = random.uniform(0.7 * pi/2, 1.3 * pi/2)
+    
+            # Update matrices
+            bpy.context.view_layer.update()
+    
+            # Debug
+            print('>', i, ' ' , n, '<')
+            print(p.location)
+            print(p.rotation_euler)
+    
+            # Render and save image if it fully visible
+            if check_projection(c, p):
+                print('True')
+                
+                render_filename = 'render-{:04d}.jpg'.format(n)
+                render_path = os.path.join(pattern_dir, render_filename)
+                
+                bpy.context.scene.render.filepath = render_path
+                bpy.ops.render.render(write_still=True)
+            
+                renders_info[render_filename] = {'pattern': 
+                                                    {'location': [p.location.x, p.location.y, p.location.z],
+                                                        'rotation': [p.rotation_euler[0], p.rotation_euler[1], p.rotation_euler[2]]
+                                                    }
+                                                }
+                n += 1
+
+            else:
+                print('False')
+            
+            i +=1 
+    
+        dataset_info['camera'] = camera_info
+        dataset_info['renders'] = renders_info
+        
+        with open(os.path.join(pattern_dir, 'info.yaml'), 'w') as info_file:
+            json.dump(dataset_info, info_file, indent=4) 
+        
