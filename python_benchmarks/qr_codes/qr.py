8c982c8bb817323687e52264b609fb3870983ac9
AleksandrPanov <alexander.panov@xperience.ai>
2022-06-21 18:56:29 +0300

    add class DetectorQR

--- python_benchmarks/qr_codes/qr.py
+++ python_benchmarks/qr_codes/qr.py
@@ -7,43 +7,71 @@ python qr.py -o out.yaml -p qrcodes/detection
 -o, --output - output file (default out.yaml)
 -p, --path - input dataset path (default qrcodes/detection)
 -a, --accuracy - input accuracy (default 47)
--alg - input alg (default OpenCV)
+-alg, --algorithm - input alg (default opencv)
 -m, --metric - input metric (default ~)
 """
 
 import argparse
 import glob
-import sys
+from enum import Enum
 
-import cv2 as cv
 import numpy as np
+import cv2 as cv
 
 
-def init_qr_detector_open_cv():
-    return cv.QRCodeDetector()
+class DetectorQR:
+    TypeDetector = Enum('TypeDetector', 'opencv opencv_wechat')
+    detected_corners = None
+    decoded_info = None
+    detector = None
+    type_detector = None
 
+    def __init__(self, type_detector=TypeDetector.opencv, path_to_model="/home/alexander/all/pycharm/master/"):
+        self.type_detector = type_detector
+        if type_detector == self.TypeDetector.opencv:
+            self.detector = cv.QRCodeDetector()
+        elif type_detector == self.TypeDetector.opencv_wechat:
+            self.detector = cv.wechat_qrcode_WeChatQRCode(path_to_model + "detect.prototxt",
+                                                          path_to_model + "detect.caffemodel",
+                                                          path_to_model + "sr.prototxt",
+                                                          path_to_model + "sr.caffemodel")
+        else:
+            raise TypeError()
 
-def init_wechat_qr_detector_open_cv(path_to_model):
-    return cv.wechat_qrcode.WeChatQRCode(path_to_model+'/detect.prototxt', path_to_model+'/detect.caffemodel',
-                                         path_to_model+'sr.caffemodel')
+    def detect(self, image):
+        if self.type_detector == self.TypeDetector.opencv:
+            ret, corners = self.detector.detectMulti(image)
+            self.detected_corners = corners
+            return ret, corners
+        elif self.TypeDetector.opencv_wechat:
+            decoded_info, corners = self.detector.detectAndDecode(image)
+            if len(decoded_info) == 0:
+                return False, None
+            corners = np.array(corners).reshape(-1, 4, 2)
+            self.decoded_info = decoded_info
+            self.detected_corners = corners
+            return True, corners
+        else:
+            raise TypeError()
 
-
-def detect_qr_opencv(image_path, detector):
-    image = cv.imread(image_path, cv.IMREAD_IGNORE_ORIENTATION)
-    #ret, corners = detector.detect(image)
-    ret, corners = detector.detectMulti(image)
-    return ret, corners
-
-
-def detect_wechat_qr_opencv(image_path, detector):
-    image = cv.imread(image_path, cv.IMREAD_IGNORE_ORIENTATION)
-    ret, corners = detector.detectAndDecode(image)
-    return ret, corners
+    def decode(self, image):
+        if self.type_detector == self.TypeDetector.opencv:
+            if self.detected_corners is None or self.detected_corners.size == 0:
+                return 0, None, None
+            r, decoded_info, straight_qrcode = self.detector.decodeMulti(image, self.detected_corners)
+            self.decoded_info = decoded_info
+            return r, decoded_info, straight_qrcode
+        elif self.TypeDetector.opencv_wechat:
+            if self.decoded_info is None or len(self.decoded_info) == 0:
+                return 0, None, None
+            return True, self.decoded_info, self.detected_corners
+        else:
+            raise TypeError()
 
 
 def find_images_path(dir_path):
-    images = glob.glob(dir_path+'/*.jpg')
-    images += glob.glob(dir_path+'/*.png')
+    images = glob.glob(dir_path + '/*.jpg')
+    images += glob.glob(dir_path + '/*.png')
     return images
 
 
@@ -54,7 +82,7 @@ def get_corners(label_path):
         try:
             f_list = [float(i) for i in line.split(" ")]
             corners += f_list
-        except:
+        except ValueError as e:
             pass
     return np.array(corners).reshape(-1, 4, 2)
 
@@ -82,7 +110,8 @@ def get_distance_to_rotate_qr(gold_corner, corners, accuracy):
                 if dist > accuracy:
                     one_corners = np.roll(one_corners, 1, 0)
                     dist = min(dist, get_distance(gold_corner, one_corners))
-                else: return dist
+                else:
+                    return dist
     return dist
 
 
@@ -95,8 +124,12 @@ def main():
     parser = argparse.ArgumentParser(description="bench QR code dataset", add_help=False)
     parser.add_argument("-H", "--help", help="show help", action="store_true", dest="show_help")
     parser.add_argument("-o", "--output", help="output file", default="out.svg", action="store", dest="output")
-    parser.add_argument("-p", "--path", help="input dataset path", default="qrcodes/detection", action="store", dest="path")
-    parser.add_argument("-a", "--accuracy", help="input accuracy", default="47", action="store", dest="accuracy", type=int)
+    parser.add_argument("-p", "--path", help="input dataset path", default="qrcodes/detection", action="store",
+                        dest="path")
+    parser.add_argument("-a", "--accuracy", help="input accuracy", default="20", action="store", dest="accuracy",
+                        type=int)
+    parser.add_argument("-alg", "--algorithm", help="QR detect algorithm", default="opencv", action="store",
+                        dest="algorithm", type=str)
 
     args = parser.parse_args()
     show_help = args.show_help
@@ -106,50 +139,61 @@ def main():
     output = args.output
     path = args.path
     accuracy = args.accuracy
+    algorithm = args.algorithm
 
-
-    listDirs = glob.glob(path+"/*")
+    list_dirs = glob.glob(path + "/*")
     fs = cv.FileStorage("out.yaml", cv.FILE_STORAGE_WRITE)
     fs.write("path", path)
     gl_qr_count = 0
     gl_qr_detect = 0
+    gl_decode = 0
 
-    detector = init_qr_detector_open_cv()
-    
-    for dir in listDirs:
-        imgsPath = find_images_path(dir)
+    qr = DetectorQR(DetectorQR.TypeDetector[algorithm], "/home/alexander/all/pycharm/master/")
+
+    for dir in list_dirs:
+        imgs_path = find_images_path(dir)
         qr_count = 0
         qr_detect = 0
-        for imgPath in imgsPath:
-            labelPath = imgPath[:-3] + "txt"
-            gold_corners = get_corners(labelPath)
-            if gold_corners.shape[0] == 1 or True:
-                qr_count += gold_corners.shape[0]
-                ret, corners = detect_qr_opencv(imgPath, detector)
+        qr_decode = 0
+        for img_path in imgs_path:
+            label_path = img_path[:-3] + "txt"
+            gold_corners = get_corners(label_path)
+            qr_count += gold_corners.shape[0]
 
-                img_name = imgPath[:-4].replace('\\', '_')
-                img_name = img_name.replace('/', '_')
+            image = cv.imread(img_path, cv.IMREAD_IGNORE_ORIENTATION)
+            ret, corners = qr.detect(image)
+            img_name = img_path[:-4].replace('\\', '_')
+            img_name = img_name.replace('/', '_')
+            fs.startWriteStruct(img_name, cv.FILE_NODE_MAP)
+            fs.write("bool", int(ret))
+            fs.write("gold_corners", gold_corners)
+            fs.write("corners", corners)
+            if ret is True:
+                i = 0
+                r, decoded_info, straight_qrcode = qr.decode(image)
+                if len(decoded_info) > 0:
+                    for info in decoded_info:
+                        if info != "":
+                            qr_decode += 1
+                fs.write("decoded_info", decoded_info)
+                for one_gold_corners in gold_corners:
+                    dist = get_distance_to_rotate_qr(one_gold_corners, corners, accuracy)
+                    fs.write("dist_to_gold_corner_" + str(i), dist)
+                    if dist <= accuracy:
+                        qr_detect += 1
+                    i += 1
+            # qr_detect += ret
+            fs.endWriteStruct()
 
-                fs.startWriteStruct(img_name, cv.FILE_NODE_MAP)
-                fs.write("bool", int(ret))
-                fs.write("gold_corners", gold_corners)
-                fs.write("corners", corners)
-                if ret is True:
-                    i = 0
-                    for one_gold_corners in gold_corners:
-                        dist = get_distance_to_rotate_qr(one_gold_corners, corners, accuracy)
-                        fs.write("dist_to_gold_corner_"+str(i), dist)
-                        if dist <= accuracy:
-                            qr_detect += 1
-                        i += 1
-                fs.endWriteStruct()
-        #print(dir, qr_count, qr_detect, qr_detect / max(1, qr_count))
-        print(dir, qr_detect / max(1, qr_count))
+        # print(dir, qr_count, qr_detect, qr_detect / max(1, qr_count))
+        print(dir, qr_detect / max(1, qr_count), qr_decode / max(1, qr_count), qr_count)
         gl_qr_count += qr_count
         gl_qr_detect += qr_detect
+        gl_decode += qr_decode
     print(gl_qr_count)
     print(gl_qr_detect)
-    print(gl_qr_detect/gl_qr_count)
+    print(gl_qr_detect / gl_qr_count)
+    print("decode", gl_decode / gl_qr_count)
 
 
 if __name__ == '__main__':
